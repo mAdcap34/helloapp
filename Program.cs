04eff78afbc97d87e8a5e7baefc09b66ed20beee
black <black@example.com>
2026-07-11 22:05:59 +0500

    Change greeting to hello everyone

--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@
 
 void SayHello()
 {
+<<<<<<< HEAD
     Console.WriteLine("hello");
 }
 
 void GreetWhite()
 {
     Console.WriteLine("hello white");
+=======
+    Console.WriteLine("hello everyone");
+>>>>>>> 2e3186f (Change greeting to hello everyone)
 }
